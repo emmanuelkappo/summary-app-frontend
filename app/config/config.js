06afee976ae9d21bf041c786fa4b462491599555
Emmanuel Kappo <emmanuel.kappoo@gmail.com>
2024-04-24 03:23:17 +0100

    update

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,4 @@
-export const CONFIG= {
-    API_ROOT: 'http://localhost:8000',
-}
+export const CONFIG = {
+  // API_ROOT: 'http://localhost:8000',
+  API_ROOT: "https://backend-t3fp.onrender.com/",
+};
